2f2f2ad1f280a3323072e16c695fda1895231810
Adam Bretz <arbretz@gmail.com>
2014-09-08 17:13:27 -0400

    Alpha version complete

--- README.md
+++ README.md
@@ -2,10 +2,85 @@
 
 Basic interface for [good](https://github.com/hapijs/good) reporter plugins.
 
+![Build Status](https://travis-ci.org/hapijs/good-reporter.svg?branch=master) ![Current Version](https://img.shields.io/npm/v/good-reporter.svg)
 
+Lead Maintainer: [Adam Bretz](https://github.com/arb)
 
 ## Usage
 
-This is an abstraction module for implementing reporters for the [good](https://github.com/hapijs/good) process monitor plugin. You won't need to interact with this module directly unless you plan on writing a custom reporting module.
+This is an abstraction module for implementing reporters for the [good](https://github.com/hapijs/good) process monitor plugin. It is generally used as a base object and varioues methods are over written for different reporters.
 
-## Writing a Good Reporter
+## Good Reporter
+### new GoodReporter ([options])
+
+creates a new GoodReporter object with the following arguments
+- `[options]` - optional arguments object
+	- `[events]` - an object of key value paris. Defaults to `{ request: [], log: [] }`.
+		- `key` - one of ("request", "log", "error", or "ops") indicating the hapi event to subscribe to
+		- `value` - an array of tags to filter incomming events. An empty array indicates no filtering.
+
+### `GoodReporter` methods
+- `start(callback)` - starts the reporter. Any "run once" logic should be in the start method. For example, creating a database connection.
+- `stop(callback)` - stops the reporter. Should be used for any tear down logic such as clearing time outs or disconnecting from a database.
+- `_filter(event, eventData)` - _private_ method for filtering incoming events. Looks into `options.events` for a match on `event` and then further filters by the tags compared to `eventData.tags`. Returns `true` if the `event` + `[eventData.tags]` should be enqueued. You should never need to call `_filter` directly.
+- `queue(event, eventData)` - adds `eventData` to the internal event queue (`_events`) if `_filter` returns `true`.
+
+## Examples
+
+Every new reporter *must* implement:
+
+1. A `report(callback)` method. This is where the logic of exactly how this reporter moves data from the internal event queue to a destination lives. Execute the callback at the end of processing. Unless you want to continue to rebroadcast the same events, empty the event queue before running `callback`.
+
+Everything else can be optional depending on the specific transmission method of `report`.
+
+### "One Off" object
+
+Below is a simple "one off" good-reporter object.
+
+```javascript
+var GoodReporter = require('good-reporter');
+
+var reporter = new GoodReporter();
+reporter.report = function (callback) {
+	for (var i = 0, il = this._events.length; i < il; ++i) {
+
+ 		var event = this._events[i];
+		console.info(JSON.parse(event));
+	}
+};
+```
+
+### Reusable Reporter
+
+If you are looking to create a custom and reusable reporter for [good](https://github.com/hapijs/good), your new object needs to inherit from `good-reporter`. You will also need to implement a `report(callback)` as well.
+
+```javascript
+var GoodReporter = require('good-reporter');
+var Util = require('util');
+
+var internals = {};
+
+module.exports = internals.GoodTwitter = function (options) {
+
+	GoodReporter.call(this, options);
+
+	this._hashTag = "#goodlogs";
+	this._account = "hapijs";
+};
+
+Hoek.inherits(internals.GoodFile, GoodTwitter);
+
+internals.GoodTwitter.prototype.stop = function (callback) {
+
+	// Send a final Tweet for the day, then close the open connection
+	callback(null);
+};
+
+
+internals.GoodTwitter.prototype.report = function (callback) {
+	// Post your good logs to your twitter account
+	callback(null);
+};
+```
+
+In this example, you need to call the `GoodReporter` constructor to set up the internal state. Afterward you can write any custom logic that your specific broadcaster needs.
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
-var GoodBox = require('../');
+var GoodReporter = require('..');
 
 var describe = lab.describe;
 var it = lab.it;
@@ -8,100 +8,167 @@ var before = lab.before;
 var after = lab.after;
 var expect = Lab.expect;
 
-describe('good-box', function () {
+describe('good-reporter', function () {
 
-    it('throws an error without using new', function(done) {
+	it('throws an error without using new', function(done) {
 
-        expect(function() {
+		expect(function() {
 
-            var box = GoodBox();
-        }).to.throw('GoodBox must be created with new');
+			var reporter = GoodReporter();
+		}).to.throw('GoodReporter must be created with new');
 
-        done();
-    });
+		done();
+	});
 
-    it('provides a start function', function (done) {
+	it('provides a start function', function (done) {
 
-        var box = new GoodBox();
-        expect(box.start).to.exist;
+		var reporter = new GoodReporter();
+		expect(reporter.start).to.exist;
 
-        box.start(function (error) {
-            expect(error).to.not.exist;
-            done();
-        });
-    });
+		reporter.start(function (error) {
+			expect(error).to.not.exist;
+			done();
+		});
+	});
 
-    it('provides a stop function', function (done) {
+	it('provides a stop function', function (done) {
 
-        var box = new GoodBox();
-        expect(box.stop).to.exist;
+		var reporter = new GoodReporter();
+		expect(reporter.stop).to.exist;
 
-        box.stop(function (error) {
-            expect(error).to.not.exist;
-            done();
-        });
-    });
+		reporter.stop(function (error) {
+			expect(error).to.not.exist;
+			done();
+		});
+	});
 
-    describe('filter', function () {
+	it('converts falsy values to empty tag arrays', function (done) {
 
-        it('returns true if this reporter should report this event type', function (done) {
+		var reporter = new GoodReporter({
+			events: {
+				error: null
+			}
+		});
 
-            var box = new GoodBox({});
-            expect(box.filter('log')).to.equal(true);
-            done();
-        });
+		expect(reporter._events.error).to.deep.equal([]);
+		done();
+	});
 
-        it('returns false if this report should not report this event type', function (done) {
+	describe('#_filter', function () {
 
-            var box = new GoodBox({
-                tags: ['ops']
-            });
-            expect(box.filter('request')).to.equal(false);
-            done();
-        });
+		it('returns true if this reporter should report this event type', function (done) {
 
-        it('returns false if the tag is missing', function (done) {
+			var reporter = new GoodReporter();
 
-            var box = new GoodBox();
-            expect(box.filter()).to.equal(false);
-            done();
-        });
-    });
+			expect(reporter._filter('log', {
+				tags: ['request', 'server', 'error', 'hapi']
+			})).to.equal(true);
 
-    describe('report', function () {
+			done();
+		});
 
-        it('logs a message to the console if the tag event matches', function (done) {
+		it('returns false if this report should not report this event type', function (done) {
 
-            var log = console.log;
+			var reporter = new GoodReporter();
 
-            console.log = function (value) {
+			expect(reporter._filter('ops', {
+				tags: []
+			})).to.equal(false);
 
-                console.log = log;
-                expect(value).to.equal('example log');
-                done();
-            };
-            var box = new GoodBox({
-                tags: ['ops']
-            });
+			done();
+		});
 
-            box.report('ops', 'example log', function (error) {
+		it('returns true if the event is matched, but there are not any tags with the data', function (done) {
 
-                expect(error).to.not.exist;
-            });
-        });
+			var reporter = new GoodReporter();
+			expect(reporter._filter('log', {
+				tags:[]
+			})).to.equal(true);
 
-        it('does not report a message if the tag even does not match', function (done) {
+			done();
+		});
 
+		it('returns false if the event is not matched', function (done) {
 
-            var box = new GoodBox({
-                tags: ['ops']
-            });
+			var reporter = new GoodReporter();
+			expect(reporter._filter('ops', {
+				tags:[]
+			})).to.equal(false);
 
-            box.report('report', 'example log', function (error) {
+			done();
+		});
 
-                expect(error).to.not.exist;
-                done();
-            });
-        });
-    });
+		it('returns false if the subscriber has tags, but the matched event does not have any', function (done) {
+
+			var reporter = new GoodReporter({
+				events: {
+					error: ['db']
+				}
+			});
+
+			expect(reporter._filter('error', {
+				tags:[]
+			})).to.equal(false);
+
+			done();
+		});
+
+		it('returns true if the event and tag match', function (done) {
+
+			var reporter = new GoodReporter({
+				events: {
+					'error': ['high', 'medium', 'log']
+				}
+			});
+
+			expect(reporter._filter('error', {
+				tags: ['hapi', 'high', 'db', 'severe']
+			})).to.equal(true);
+			done();
+		});
+
+		it('returns false by defualt', function (done) {
+
+		   var reporter = new GoodReporter({
+			   events: {
+				   request: ['hapi']
+			   }
+		   });
+
+			expect(reporter._filter('request',{})).to.equal(false);
+			done();
+		});
+	});
+
+	describe('#queue', function () {
+
+		it('adds an item to the internal list if _filter returns true', function (done) {
+
+			var reporter = new GoodReporter({
+				events: {
+					'error': []
+				}
+			});
+
+			reporter.queue('error', { data: 'some event data'});
+
+			expect(reporter._eventQueue.length).to.equal(1);
+			done();
+		});
+
+		it('doe not add an item to the internal list if _filter returns false', function (done) {
+
+			var reporter = new GoodReporter({
+				events: {
+					request: []
+				}
+			});
+
+			reporter.queue('error', { data: 'some event data'});
+
+			expect(reporter._eventQueue.length).to.equal(0);
+			done();
+		});
+
+	});
 });
